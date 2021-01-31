a21a92f23b00ddd3ddfc9666d7473075a79dfd5f
Aleksandra Semikozova <aleksandrasemikozova@gmail.com>
2021-01-31 12:33:45 +0200

    Исправить ошибки

--- js/script.js
+++ js/script.js
@@ -2,43 +2,30 @@ let popup = document.querySelector('.popup');
 let popupOpenButton = document.querySelector('.profile__edit-btn');
 let popupCloseButton = popup.querySelector('.popup__close-icon');
 let formElement = popup.querySelector('.popup__form');
-let nameInput = popup.querySelector('.popup__item-name');
-let jobInput = popup.querySelector('.popup__item-job');
+let nameInput = popup.querySelector('.popup__item_name');
+let jobInput = popup.querySelector('.popup__item_job');
+let formNameInput = formElement.querySelector('.popup__item_name');
+let formJobInput = formElement.querySelector('.popup__item_job');
+let name = document.querySelector('.profile__title');
+let job = document.querySelector('.profile__subtitle');
 
-
-popupOpenButton.addEventListener("click", function() {
-          if (!popup.classList.contains("popup_opened")) {
-              popup.classList.add("popup_opened");
-          } else {
-              popup.classList.remove("popup_opened");
-          }
-   });
-
-   popupCloseButton.addEventListener("click", function() {
-    if (!popup.classList.contains("popup_opened")) {
-        popup.classList.add("popup_opened");
-    } else {
-        popup.classList.remove("popup_opened");
-    }
-  });
-
-popupOpenButton.addEventListener('click', function(){
-});
-popupCloseButton.addEventListener('click', function(){
-});
-
-function formSubmitHandler (evt) {
-  evt.preventDefault();
-  let name = document.querySelector('.profile__title');
-  let job = document.querySelector('.profile__subtitle');
-
-    nameInput.textContent = (nameInput.value)
-    jobInput.textContent = (jobInput.value)
-
-    name.textContent = (nameInput.value);
-    job.textContent = (jobInput.value);
+function openPopup() {
+  popup.classList.add('popup_opened');
+  nameInput.value = name.textContent;
+  jobInput.value = job.textContent;
 }
 
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  name.textContent = nameInput.value;
+  job.textContent = jobInput.value;
+  closePopup();
+}
+
+popupOpenButton.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
-
-
