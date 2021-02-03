7406a7d5f5509dce05883519d214ac1d24b3e40e
Aleksandra Semikozova <aleksandrasemikozova@gmail.com>
2021-02-03 13:22:46 +0200

    Удалить повторяющиеся переменные, исправить их имена

--- js/script.js
+++ js/script.js
@@ -2,17 +2,15 @@ let popup = document.querySelector('.popup');
 let popupOpenButton = document.querySelector('.profile__edit-btn');
 let popupCloseButton = popup.querySelector('.popup__close-icon');
 let formElement = popup.querySelector('.popup__form');
-let nameInput = popup.querySelector('.popup__item_name');
-let jobInput = popup.querySelector('.popup__item_job');
-let formNameInput = formElement.querySelector('.popup__item_name');
-let formJobInput = formElement.querySelector('.popup__item_job');
-let name = document.querySelector('.profile__title');
-let job = document.querySelector('.profile__subtitle');
+let formNameInput = formElement.querySelector('.popup__item_type_user-name');
+let formJobInput = formElement.querySelector('.popup__item_type_user-job');
+let profileNameElement = document.querySelector('.profile__title');
+let profileJobElement = document.querySelector('.profile__subtitle');
 
 function openPopup() {
   popup.classList.add('popup_opened');
-  nameInput.value = name.textContent;
-  jobInput.value = job.textContent;
+  formNameInput.value = profileNameElement.textContent;
+  formJobInput.value = profileJobElement.textContent;
 }
 
 function closePopup() {
@@ -21,8 +19,8 @@ function closePopup() {
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
-  name.textContent = nameInput.value;
-  job.textContent = jobInput.value;
+  profileNameElement.textContent = formNameInput.value;
+  profileJobElement.textContent = formJobInput.value;
   closePopup();
 }
 
